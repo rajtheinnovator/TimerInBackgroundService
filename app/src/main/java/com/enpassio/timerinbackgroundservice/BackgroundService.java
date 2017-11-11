66a261778c798f2c955b0a1191fa1bb23bc0c651
ABHISHEK RAJ <rajtheinnovatorabhishek@gmail.com>
2017-11-11 23:11:58 +0530

    Showing location only after 20 seconds

--- app/src/main/java/com/enpassio/timerinbackgroundservice/BackgroundService.java
+++ app/src/main/java/com/enpassio/timerinbackgroundservice/BackgroundService.java
@@ -2,16 +2,30 @@ package com.enpassio.timerinbackgroundservice;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
 /**
  * Created by ABHISHEK RAJ on 11/11/2017.
  */
 
-public class BackgroundService extends IntentService {
+public class BackgroundService extends IntentService implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
 
     private static final String TAG = "BackgroundService";
 
@@ -19,18 +33,68 @@ public class BackgroundService extends IntentService {
         super(TAG);
     }
 
+    GoogleApiClient mGoogleApiClient;
+    LocationRequest mLocationRequest;
+    Location mLocation;
+    private boolean isReadyToBeSaved = false;
+
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
         Log.v("my_tag", "onHandleIntent called");
         final Handler handler = new Handler(Looper.getMainLooper());
         final Runnable r = new Runnable() {
             public void run() {
-                Log.v("my_tag", "tagggggg");
-                handler.postDelayed(this, 1000);
+                Log.v("my_tag", "handler runs");
+                handler.postDelayed(this, 20000);
+                isReadyToBeSaved = true;
             }
         };
-        handler.postDelayed(r, 1000);
+        handler.postDelayed(r, 20000);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(10 * 1000)        // 10 seconds, in milliseconds
+                .setFastestInterval(1 * 1000); // 1 second, in milliseconds
+        mGoogleApiClient.connect();
     }
 
 
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        } else {
+            if (mLocation == null) {
+                LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+            } else {
+                mLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            }
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mLocation = location;
+        Log.v("my_tag", "onLocationChanged runs with isReadyToBeSaved: " + isReadyToBeSaved);
+        if (isReadyToBeSaved) {
+            Toast.makeText(this, "location is: " + location.getLongitude(), Toast.LENGTH_SHORT).show();
+        }
+        isReadyToBeSaved = false;
+    }
 }
